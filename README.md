9c3b1d4a6eeeef56b658e68381b4ff0cab76e7f9
OmenByHp <tomoakitaro777@gmail.com>
2018-03-23 15:00:23 +0900

    tmp

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-## Synopsis
-
-https://coinmarketcal.com/　のスクレイピング
-
-## Code Example
+## 目的
 
 
+[Crytocurrency Calendar][1]のスクレイピング。サイトからそれぞれのイベントの時間、タイトル、種類、内容をとってきて、MySQLに格納する
+
+![[Crytocurrency Calendar][1](crypto_cal.png)]{style="display:block;margin-left:auto;margin-right:auto;"}
+
+##　ファイルの説明
+
+test.py ~ test4.py
+XPathを使いサイトから
 
 ## Motivation
 
@@ -17,3 +21,6 @@ Describe and show how to run the tests with code examples.
 ## License
 
 MIT
+
+
+[1]:https://coinmarketcal.com/
--- test2.py
+++ test2.py
@@ -1,41 +1,25 @@
+# 1ページのサイトからすべてのイベントを持ってくる
 import requests
 import lxml.html
-import MySQLdb
-
-# データベースに接続する
-connection = MySQLdb.connect(
-    user="scrapingman",
-    passwd="dxdutch",
-    host="localhost",
-    db="crypto_cal_data",
-    charset="utf8"
-)
-
-cursor = connection.cursor()
-
-# 実行ごとにテーブルを削除する
-cursor.execute("DROP TABLE IF EXISTS books")
-# テーブル作成
-cursor.execute(
-    "CREAT TABLE books (time text, title text, sort text, content text)")
 
 r = requests.get("https://coinmarketcal.com/")
 html = r.text
 root = lxml.html.fromstring(html)
 
-time_of_event = root.xpath(
-    "/html/body/main/div[3]/section[1]/div[2]/div[3]/article[1]/div/h5[1]/strong")
+event = root.xpath(
+    "/html/body/main/div[3]/section[1]/div[2]/div[3]")
 
-print(time_of_event[0].text.strip())
-title_of_event = root.xpath(
-    "/html/body/main/div[3]/section[1]/div[2]/div[3]/article[1]/div/h5[2]/strong")
+time_of_event = event[0].xpath("article[1]/div/h5[1]/strong")
+print(time_of_event[0].text)
+
+title_of_event = event[0].xpath(
+    "article[1]/div/h5[2]/strong")
 print(title_of_event[0].text.strip())
-sort_of_event = root.xpath(
-    "/html/body/main/div[3]/section[1]/div[2]/div[3]/article[1]/div/h5[3]")
+
+sort_of_event = event[0].xpath(
+    "article[1]/div/h5[3]")
 print(sort_of_event[0].text.strip())
-content_of_event = root.xpath(
-    "/html/body/main/div[3]/section[1]/div[2]/div[3]/article[1]/div/div[1]/p[2]")
+
+content_of_event = event[0].xpath(
+    "article[1]/div/div[1]/p[2]")
 print(content_of_event[0].text.strip())
-
-
-cursor.execute("INSERT INTO books VALUES()")
